8846530c22ea21de604cb9999f9184063b42b831
EdShel <59361045+EdShel@users.noreply.github.com>
2020-03-12 19:28:23 +0200

    Created solution with Web-parser, added class PageParser with methods of executing CSS-like queries and parsing float-value variables from string. There is a DEMO in Program.cs

---
+++ SalesParser/Parsers/PageParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SalesParser.Parsers
+{
+    /// <summary>
+    /// Class designed to get sales data from Web pages
+    /// via parsing their HTML content
+    /// </summary>
+    public class PageParser
+    {
+        /// <summary>
+        /// Structure of the loaded HTML document
+        /// </summary>
+        private HtmlDocument pageDOM;
+
+        /// <summary>
+        /// Regular expression pattern for parsing pieces of prices
+        /// </summary>
+        private const string DEFAULT_PRICE_REG_EX = @"(\d+)([<>\w]*(\d\d))?";
+
+        /// <summary>
+        /// Regular expression for parsing pieces of prices
+        /// </summary>
+        private static readonly Regex priceRegex = new Regex(DEFAULT_PRICE_REG_EX);
+
+        /// <summary>
+        /// Creates new parser using stream with HTML code.
+        /// </summary>
+        /// <remarks></remarks>
+        /// <param name="pageHtmlCode">Stream with HTML code of the
+        /// desired page. For example, it can use System.Net.WebResponce stream.</param>
+        public PageParser(Stream pageHtmlCode)
+        {
+            // Save document structure
+            pageDOM = new HtmlDocument();
+            pageDOM.Load(pageHtmlCode, false);
+        }
+
+        /// <summary>
+        /// Executes CSS query selector and returns found document nodes
+        /// </summary>
+        /// <param name="querySelector">CSS-like selector. For example,
+        /// ".className div #classId"</param>
+        public IEnumerable<HtmlNode> ParseWebPage(string querySelector)
+        {
+            var document = pageDOM.DocumentNode;
+            return document.QuerySelectorAll(querySelector);
+        }
+
+        /// <summary>
+        /// Parses string in order to get price as float value
+        /// </summary>
+        /// <param name="elementHtml">InnerHTML of parsed node 
+        /// or simply any string that contains number.</param>
+        /// <returns>Returns null if the argument does not match
+        /// priceRegex regular expression or float valur if it does</returns>
+        public static float? ParsePrice(string elementHtml)
+        {
+            var priceSearch = priceRegex.Match(elementHtml); // find all the pieces of price
+            var numberParts = priceSearch.Groups;
+
+            if (numberParts.Count == 2          // found only 1) int price
+               || numberParts.Count == 4)       // found following constructions:
+                                                // 1) int price
+                                                // 2) additional part with coins
+                                                // 3) pennies
+            {
+                // Parsing as int 'cause we don't want it to have any kind of comas and dots
+                float price = Int32.Parse(
+                    s: numberParts[1].Value);
+
+                // If there is also pennies part
+                if (numberParts.Count >= 4)
+                {
+                    // Add pennies to the price
+                    price += (float)Int32.Parse(s: numberParts[3].Value) / 100;
+                }
+
+                return price;
+            }
+
+            return null;
+        }
+    }
+}
+
---
+++ SalesParser/Program.cs
@@ -0,0 +1,48 @@
+﻿using SalesParser.Parsers;
+using System;
+using System.Net;
+
+namespace SalesParser
+{
+    class Program
+    {
+        const string URL = "https://www.atbmarket.com/ru/hot/akcii/7day/";
+        const string QUERY = @"body .promo_price";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading site content...");
+
+            // Ask server for a HTML page
+            HttpWebRequest oReq = (HttpWebRequest)WebRequest.Create(URL);
+            HttpWebResponse resp = (HttpWebResponse)oReq.GetResponse();
+
+            Console.WriteLine("HTML downloaded, parsing...");
+
+            // Create price parser
+            var parser = new PageParser(resp.GetResponseStream());
+            
+            Console.WriteLine($"Executing query: {QUERY}");
+
+            // Go through all the occurences of CSS-like selector
+            foreach (var res in parser.ParseWebPage(QUERY))
+            {
+                // Output the occurence
+                Console.WriteLine(res.InnerHtml);
+
+                // Try to parse the price from it
+                var price = PageParser.ParsePrice(res.InnerHtml);
+                if (price != null)
+                {
+                    Console.WriteLine($"Read price: {price.Value:f2}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("App has finished its execution");
+            Console.Read();
+        }
+    }
+}
+
