960eec8aebdb3e9c6e1947900f962e6f81b9ff97
EdShel <eduardchelemetjev@gmail.com>
2020-05-27 09:30:18 +0300

    Added background task scheduling for parsing using Hangfire framework

--- GainBargain.WEB/Global.asax.cs
+++ GainBargain.WEB/Global.asax.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Principal;
 using System.Web;
@@ -7,6 +9,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using GainBargain.DAL.EF;
+using Hangfire;
+using Hangfire.SqlServer;
 
 namespace GainBargain.WEB
 {
@@ -18,6 +22,20 @@ namespace GainBargain.WEB
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            HangfireAspNet.Use(GetHangfireServers);
+
+            // Cron schedule
+            // minutes
+            // hours
+            // day of the month 1-31
+            // month
+            // day of the week 0-6
+            //RecurringJob.AddOrUpdate("parsing", () => Models.Parser.Start(), "59 8 * * *");
+
+            // Start the parsing after 1 minute
+            DateTime now = DateTime.UtcNow;
+            RecurringJob.AddOrUpdate("parsing", () => Models.Parser.Start(), $"{now.Minute + 1} {now.Hour} * * *");
         }
 
         //protected void Application_AuthenticateRequest()
@@ -48,5 +66,34 @@ namespace GainBargain.WEB
         //    Context.User = newUserObj;
 
         //}
+
+        /// <summary>
+        /// This method is used for creating background
+        /// tasks (in our case is parsing)
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            var conStr = ConfigurationManager.ConnectionStrings["GainBargainContext"].ConnectionString;
+
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(conStr, new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    UsePageLocksOnDequeue = true,
+                    DisableGlobalLocks = true,
+                });
+
+            yield return new BackgroundJobServer(new BackgroundJobServerOptions
+            {
+                WorkerCount = Models.Parser.MAX_PROCESSING_SOURCES
+            });
+        }
     }
 }
--- GainBargain.WEB/Models/Parser.cs
+++ GainBargain.WEB/Models/Parser.cs
@@ -1,14 +1,138 @@
-﻿using GainBargain.DAL.Entities;
+﻿using GainBargain.DAL.EF;
+using GainBargain.DAL.Entities;
+using GainBargain.DAL.Interfaces;
+using GainBargain.DAL.Repositories;
 using GainBargain.Parser.Parsers;
 using GainBargain.Parser.WebAccess;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GainBargain.WEB.Models
 {
-    public static class Parser
+    public class Parser
     {
-        public static async Task<IEnumerable<Product>> ParseAsync(ParserSource source)
+        public static int MAX_PROCESSING_SOURCES = 5;
+
+        public static ParsingState ParsingProgress = new ParsingState();
+
+        private GainBargainContext db = new GainBargainContext();
+        private IParserSourceRepository parserSourceRepository;
+        private IDbLogsRepository dbLogsRepository;
+
+        private Parser()
+        {
+            parserSourceRepository = new ParserSourceRepository(db);
+            dbLogsRepository = new DbLogsRepository(db);
+        }
+
+        public static void Start()
+        {
+            if (!ParsingProgress.IsParsing)
+            {
+                new Parser().InitParsing();
+            }
+        }
+
+        private void InitParsing()
+        {
+
+            // If we're parsing now
+            if (ParsingProgress.IsParsing)
+            {
+                // Somebody wants to start parsing again
+                return;
+            }
+
+            // Get sources to parse
+            var sources = db.ParserSources
+                .Where(s => s.CategoryId == 11)
+                .Include(s => s.Market)
+                .ToList();
+
+            try
+            {
+                // Tell the system that the parsing had started
+                ParsingProgress.ParsingStarted(sources.Count);
+                dbLogsRepository.Log(DbLog.LogCode.Info, $"Started parsing of {sources.Count} sources.");
+
+                using (SemaphoreSlim concurrencySemaphore = new SemaphoreSlim(MAX_PROCESSING_SOURCES))
+                {
+                    List<Task> parsings = new List<Task>();
+                    object parsedIncrLock = new object();
+
+                    foreach (ParserSource source in sources)
+                    {
+                        concurrencySemaphore.Wait();
+
+                        // If all threads are running
+                        parsings.Add(Task.Run(async () =>
+                        {
+                            try
+                            {
+                                // Create new context for sending batched products inserts
+                                var ctxt = new GainBargainContext();
+
+                                // Create the command for inserting products
+                                using (var productInsert = new ProductInsertCommand(ctxt))
+                                {
+                                    // Insert every parsed product
+                                    foreach (Product p in await Models.Parser.ParseAsync(source))
+                                    {
+                                        productInsert.ExecuteOn(p);
+                                    }
+                                }
+
+                                // For tracking parsing progress
+                                lock (parsedIncrLock)
+                                {
+                                    // Increment processed parsing sources count
+                                    ParsingProgress.IncrementDoneSources();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                dbLogsRepository.Log(DbLog.LogCode.Error, ex.Message);
+                            }
+                            finally
+                            {
+                                // If thread is failed, release semaphore
+                                concurrencySemaphore.Release();
+                            }
+                        }));
+                    }
+
+                    // Wait for all the tasks to be completed
+                    Task.WaitAll(parsings.ToArray());
+                }
+            }
+            finally
+            {
+                // In any case parsing must finish here
+                ParsingProgress.ParsingFinished();
+
+                dbLogsRepository.Log(DbLog.LogCode.Info, "Finished parsing. Starting omptimization.");
+
+                // Set timeout to 60 minutes
+                int? defTimeout = db.Database.CommandTimeout;
+                db.Database.CommandTimeout = 60 * 60;
+
+                // Remove already existing entries
+                db.Database.ExecuteSqlCommand("RemoveDuplicates");
+
+                // Update product's cache
+                db.Database.ExecuteSqlCommand("UpdateProductsCache");
+
+                db.Database.CommandTimeout = defTimeout;
+
+                dbLogsRepository.Log(DbLog.LogCode.Info, "Optimization is over. Parsing is done.");
+            }
+        }
+
+        private static async Task<IEnumerable<Product>> ParseAsync(ParserSource source)
         {
             // Download web request
             string url = source.Url;
