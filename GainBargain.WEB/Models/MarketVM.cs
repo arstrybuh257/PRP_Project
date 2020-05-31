19814b2566afc639e01e19d1310298dbadd63612
Valeriia Slostina <valeriia.slostina@gmail.com>
2020-06-01 02:15:18 +0300

    changed something...

--- GainBargain.WEB/Models/MarketVM.cs
+++ GainBargain.WEB/Models/MarketVM.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string MarketLogoUrl { get; set; }
+        public string Addresses { get; set; }
     }
 }
